f422cdc0541b43d18ac71601db9122ca58bbcea6
S-Tangg <skytang@outlook.com>
2019-08-13 23:05:24 +0800

    Create app.js

---
+++ day37/js/app.js
@@ -0,0 +1,35 @@
+// loadLocalStorage();
+drawLine(saleArr, '#0000FF');
+drawBar();
+funShowLocationStatue();
+renderPage();
+function renderPage() {
+    //删除table重新绘制
+    tableWrapper.innerHTML = '';
+    // if (isChangeProduct && isChangeRegion) {
+        getDate();
+        drawTable();
+        redrawLine();
+        document.getElementById('save').style.display = 'block';
+    // }
+}
+//draw line base on table
+function redrawLine() {
+    var temp = [];
+    ctx.clearRect(40, 0, 440, 229);
+    for (i = 0; i < baseOnTarget.length; i++) {
+        temp.push(baseOnTarget[i].sale);
+    }
+    for (j = 0; j < temp.length; j++) {
+        drawLine(temp[j], colorArr[j]);
+        // console.log(colorArr[j]);   
+    }
+}
+
+document.getElementById('save').onclick = function() {
+    saveLocalStorage();
+}
+window.onpopstate = function() {
+    funShowLocationStatue();
+    renderPage();
+}
